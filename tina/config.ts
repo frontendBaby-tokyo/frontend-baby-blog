b594737c17eda3da49e08be59b5b927e8c659862
frontendBaby-tokyo <192111045+frontendBaby-tokyo@users.noreply.github.com>
2025-07-20 02:48:13 +0900

    カテゴリー廃止

--- tina/config.ts
+++ tina/config.ts
@@ -1,4 +1,3 @@
-import { CATEGORIES } from '../src/data/categories.ts'
 import { defineConfig } from 'tinacms'
 
 // Your hosting provider likely exposes this as an environment variable
@@ -35,14 +34,6 @@ export default defineConfig({
 						description: 'The image used for the cover of the post'
 					},
 
-					{
-						type: 'string',
-						required: true,
-						name: 'category',
-						label: 'Category',
-						description: 'Select an category for this post',
-						options: [...CATEGORIES]
-					},
 					{
 						type: 'string',
 						label: 'description',
